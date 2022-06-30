d0813db6e753e9195a984d96725d11e00e2193d6
nwjbrandon <nwjbrandon@gmail.com>
2022-06-30 23:58:05 +0800

    feat: add crnn model

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-python3 main.py --cfg cfgs/text_detection_rec.yml
+# python3 main.py --cfg cfgs/text_detection_rec.yml
 # python3 main.py --cfg cfgs/text_detection_rec.yml --mode test
+
+python3 main.py --cfg cfgs/text_detection_rec_crnn.yml
+# python3 main.py --cfg cfgs/text_detection_rec_crnn.yml --mode test
